cc3a591fbb08682e3906a0a7cc94a07f580695a1
Nathan Smith <nathan@keyba.se>
2019-08-08 15:33:47 -0400

    Add a test for listen

--- kbchat/kbchat_test.go
+++ kbchat/kbchat_test.go
@@ -9,8 +9,10 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"strconv"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	"gopkg.in/yaml.v2"
@@ -114,7 +116,7 @@ func deleteWorkingDir(workingDir string) error {
 	return os.RemoveAll(workingDir)
 }
 
-var kbc *API
+var alice *API
 var config Config
 var channel Channel
 var teamChannel Channel
@@ -133,7 +135,7 @@ func TestMain(m *testing.M) {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error in preparing working directory: %v\n", err)
 	}
-	kbc, err = Start(RunOptions{KeybaseLocation: kbLocation, HomeDir: dir, Oneshot: config.Bots.Alice1, StartService: true})
+	alice, err = Start(RunOptions{KeybaseLocation: kbLocation, HomeDir: dir, Oneshot: config.Bots.Alice1, StartService: true})
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error in starting service: %v\n", err)
 	}
@@ -152,7 +154,7 @@ func TestMain(m *testing.M) {
 	flag.Parse()
 	code := m.Run()
 
-	err = kbc.Shutdown()
+	err = alice.Shutdown()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error on service shutdown: %v\n", err)
 	}
@@ -164,17 +166,17 @@ func TestMain(m *testing.M) {
 }
 
 func TestGetUsername(t *testing.T) {
-	require.Equal(t, kbc.GetUsername(), config.Bots.Alice1.Username)
+	require.Equal(t, alice.GetUsername(), config.Bots.Alice1.Username)
 }
 
 func TestGetConversations(t *testing.T) {
-	conversations, err := kbc.GetConversations(false)
+	conversations, err := alice.GetConversations(false)
 	require.NoError(t, err)
 	require.Greater(t, len(conversations), 0)
 }
 
 func TestGetTextMessages(t *testing.T) {
-	messages, err := kbc.GetTextMessages(channel, false)
+	messages, err := alice.GetTextMessages(channel, false)
 	require.NoError(t, err)
 	require.Greater(t, len(messages), 0)
 }
@@ -183,12 +185,12 @@ func TestSendMessage(t *testing.T) {
 	text := "test SendMessage"
 
 	// Send the message
-	res, err := kbc.SendMessage(channel, text)
+	res, err := alice.SendMessage(channel, text)
 	require.NoError(t, err)
 	require.Greater(t, res.Result.MsgID, 0)
 
 	// Read it to confirm it sent
-	messages, err := kbc.GetTextMessages(channel, false)
+	messages, err := alice.GetTextMessages(channel, false)
 	require.NoError(t, err)
 	sentMessage := messages[0]
 	require.Equal(t, sentMessage.Content.Type, "text")
@@ -200,16 +202,16 @@ func TestSendMessageByConvID(t *testing.T) {
 	text := "test SendMessageByConvID"
 
 	// Retrieve conversation ID
-	messages, err := kbc.GetTextMessages(channel, false)
+	messages, err := alice.GetTextMessages(channel, false)
 	convID := messages[0].ConversationID
 
 	// Send the message
-	res, err := kbc.SendMessageByConvID(convID, text)
+	res, err := alice.SendMessageByConvID(convID, text)
 	require.NoError(t, err)
 	require.Greater(t, res.Result.MsgID, 0)
 
 	// Read it to confirm it sent
-	messages, err = kbc.GetTextMessages(channel, false)
+	messages, err = alice.GetTextMessages(channel, false)
 	require.NoError(t, err)
 	sentMessage := messages[0]
 	require.Equal(t, sentMessage.Content.Type, "text")
@@ -221,12 +223,12 @@ func TestSendMessageByTlfName(t *testing.T) {
 	text := "test SendMessageByTlfName"
 
 	// Send the message
-	res, err := kbc.SendMessageByTlfName(channel.Name, text)
+	res, err := alice.SendMessageByTlfName(channel.Name, text)
 	require.NoError(t, err)
 	require.Greater(t, res.Result.MsgID, 0)
 
 	// Read it to confirm it sent
-	messages, err := kbc.GetTextMessages(channel, false)
+	messages, err := alice.GetTextMessages(channel, false)
 	require.NoError(t, err)
 	sentMessage := messages[0]
 	require.Equal(t, sentMessage.Content.Type, "text")
@@ -238,12 +240,12 @@ func TestSendMessageByTeamName(t *testing.T) {
 	text := "test SendMessageByTeamName"
 
 	// Send the message
-	res, err := kbc.SendMessageByTeamName(teamChannel.Name, text, &teamChannel.TopicName)
+	res, err := alice.SendMessageByTeamName(teamChannel.Name, text, &teamChannel.TopicName)
 	require.NoError(t, err)
 	require.Greater(t, res.Result.MsgID, 0)
 
 	// Read it to confirm it sent
-	messages, err := kbc.GetTextMessages(teamChannel, false)
+	messages, err := alice.GetTextMessages(teamChannel, false)
 	fmt.Printf("messages = %+v\n", messages)
 	require.NoError(t, err)
 	sentMessage := messages[0]
@@ -263,13 +265,13 @@ func TestSendAttachmentByTeam(t *testing.T) {
 
 	// Send the message
 	title := "test SendAttachmentByTeam"
-	res, err := kbc.SendAttachmentByTeam(teamChannel.Name, location, title, &teamChannel.TopicName)
+	res, err := alice.SendAttachmentByTeam(teamChannel.Name, location, title, &teamChannel.TopicName)
 	require.NoError(t, err)
 	require.Greater(t, res.Result.MsgID, 0)
 
 	// Types don't support attachments yet, so we can't read it
 	// Read it to confirm it sent
-	// messages, err := kbc.GetTextMessages(teamChannel, false)
+	// messages, err := alice.GetTextMessages(teamChannel, false)
 	// require.NoError(t, err)
 	// sentMessage := messages[0]
 	// require.Equal(t, sentMessage.Content.Type, "attachment")
@@ -280,12 +282,12 @@ func TestSendAttachmentByTeam(t *testing.T) {
 func TestReactByChannel(t *testing.T) {
 	react := ":cool:"
 	// Get last message, we'll react to it
-	messages, err := kbc.GetTextMessages(channel, false)
+	messages, err := alice.GetTextMessages(channel, false)
 	require.NoError(t, err)
 	lastMessageID := messages[0].MsgID
 
 	// Send the react
-	res, err := kbc.ReactByChannel(channel, lastMessageID, react)
+	res, err := alice.ReactByChannel(channel, lastMessageID, react)
 	require.NoError(t, err)
 	require.Greater(t, res.Result.MsgID, 0)
 
@@ -296,16 +298,17 @@ func TestReactByConvID(t *testing.T) {
 	react := ":cool:"
 
 	// Get last message, we'll react to it
-	messages, err := kbc.GetTextMessages(channel, false)
+	messages, err := alice.GetTextMessages(channel, false)
 	require.NoError(t, err)
 	lastMessageID := messages[0].MsgID
 
 	// Retrieve conversation ID
-	messages, err = kbc.GetTextMessages(channel, false)
+	messages, err = alice.GetTextMessages(channel, false)
+	require.NoError(t, err)
 	convID := messages[0].ConversationID
 
 	// Send the react
-	res, err := kbc.ReactByConvID(convID, lastMessageID, react)
+	res, err := alice.ReactByConvID(convID, lastMessageID, react)
 	require.NoError(t, err)
 	require.Greater(t, res.Result.MsgID, 0)
 }
@@ -313,7 +316,7 @@ func TestReactByConvID(t *testing.T) {
 func TestAdvertiseCommands(t *testing.T) {}
 
 func TestListChannels(t *testing.T) {
-	channels, err := kbc.ListChannels(teamChannel.Name)
+	channels, err := alice.ListChannels(teamChannel.Name)
 	require.NoError(t, err)
 	require.Greater(t, len(channels), 0)
 	channelFound := false
@@ -327,13 +330,43 @@ func TestListChannels(t *testing.T) {
 }
 
 func TestJoinAndLeaveChannel(t *testing.T) {
-	_, err := kbc.LeaveChannel(teamChannel.Name, teamChannel.TopicName)
+	_, err := alice.LeaveChannel(teamChannel.Name, teamChannel.TopicName)
 	require.NoError(t, err)
-	_, err = kbc.LeaveChannel(teamChannel.Name, teamChannel.TopicName)
+	_, err = alice.LeaveChannel(teamChannel.Name, teamChannel.TopicName)
 	require.Error(t, err)
-	_, err = kbc.JoinChannel(teamChannel.Name, teamChannel.TopicName)
+	_, err = alice.JoinChannel(teamChannel.Name, teamChannel.TopicName)
 	require.NoError(t, err)
-	_, err = kbc.JoinChannel(teamChannel.Name, teamChannel.TopicName)
+	_, err = alice.JoinChannel(teamChannel.Name, teamChannel.TopicName)
 	// We don't get an error when trying to join an already joined channel
 	require.NoError(t, err)
+}
+
+func TestListenForNewTextMessages(t *testing.T) {
+	dir, err := randomTempDir()
+	require.NoError(t, err)
+	kbLocation, err := prepWorkingDir(dir)
+	require.NoError(t, err)
+	bob, err := Start(RunOptions{KeybaseLocation: kbLocation, HomeDir: dir, Oneshot: config.Bots.Bob1, StartService: true})
+	require.NoError(t, err)
+
+	sub, err := alice.ListenForNewTextMessages()
+	require.NoError(t, err)
+
+	text := "testing listen"
+
+	go func() {
+		for i := 0; i < 5; i++ {
+			time.Sleep(3 * time.Second)
+			_, err := bob.SendMessage(channel, text+" "+strconv.Itoa(i))
+			require.NoError(t, err)
+		}
+	}()
+
+	for i := 0; i < 5; i++ {
+		msg, err := sub.Read()
+		require.NoError(t, err)
+		require.Equal(t, msg.Message.Content.Type, "text")
+		require.Equal(t, msg.Message.Sender.Username, bob.GetUsername())
+		require.Equal(t, msg.Message.Content.Text.Body, text+" "+strconv.Itoa(i))
+	}
 }
